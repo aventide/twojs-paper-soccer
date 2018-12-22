e7a43babd3cd795ec3103a3afed72129c963c994
Alex Staples <ajstaples@gmail.com>
2018-12-21 22:43:15 -0500

    draw line segments from centerpoint

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 // const config = {fullscreen: true};
 const config = {width: 450, height: 500};
 
@@ -8,33 +6,27 @@ const two = new Two(config);
 
 two.appendTo(appElem);
 
-let pos = {x: 100, y: 100};
-
-function drawGraphPaper (nRows, nCols, increment){
-    for(let x = 0; x <= nRows; x++) {
+function drawGraphPaper(nRows, nCols, increment) {
+    for (let x = 0; x <= nRows; x++) {
         const line = two.makeLine(0, x * increment, (nCols * increment), x * increment);
         line.stroke = "lightblue";
     }
-    for(let y = 0; y <= nCols; y++) {
+    for (let y = 0; y <= nCols; y++) {
         const line = two.makeLine(y * increment, 0, y * increment, (nRows * increment));
         line.stroke = "lightblue";
     }
 }
 
-function drawGoalA (nRows, nCols, increment){
+function getCenterPoint(nRows, nCols, increment) {
     const centerpointX = nCols * increment / 2;
-
-    const back = two.makeLine(centerpointX - increment, 0, centerpointX + increment, 0);
-    back.stroke = "black";
-
-    const side1 = two.makeLine(centerpointX - increment, 0, centerpointX - increment, increment);
-    const side2 = two.makeLine(centerpointX + increment, 0, centerpointX + increment, increment);
-    side1.stroke = "black";
-    side2.stroke = "black";
-
+    const centerpointY = nRows * increment / 2;
+    return {
+        x: centerpointX,
+        y: centerpointY
+    };
 }
 
-function drawPitch (nRows, nCols, increment) {
+function drawPitch(nRows, nCols, increment) {
 
     const segments = [];
 
@@ -48,7 +40,6 @@ function drawPitch (nRows, nCols, increment) {
 
     // draw goal ends
     segments.push(
-
         // top end
 
         two.makeLine(centerpointX - increment, 0, centerpointX + increment, 0),
@@ -72,40 +63,101 @@ function drawPitch (nRows, nCols, increment) {
 
         two.makeLine(0, increment, 0, heightBound - increment),
         two.makeLine(widthBound, increment, widthBound, heightBound - increment),
-
     );
-
 
 
     segments.forEach(s => s.linewidth = 3);
 
 }
 
-function drawStartDot (nRows, nCols, increment) {
-    const centerpointX = nCols * increment / 2;
-    const centerpointY = nRows * increment / 2;
-    const dot = two.makeCircle(centerpointX, centerpointY, 8);
+function drawStartDot(point) {
+    const dot = two.makeCircle(point.x, point.y, 8);
     dot.fill = "black";
+
+    return dot;
+
 }
 
-function drawLineSegment (nRows, nCols, increment) {
-    const centerpointX = nCols * increment / 2;
-    const centerpointY = nRows * increment / 2;
-    const segment = two.makeLine(centerpointX, centerpointY, centerpointX + increment, centerpointY + increment);
-    segment.stroke = "black";
-    segment.linewidth = 2;
+function drawLinePath(points) {
+    if (!points || !points.length) {
+        return;
+    }
+    let currentPoint;
+    let nextPoint;
 
-    const segment2 = two.makeLine(centerpointX + increment,  centerpointY + increment, centerpointX, centerpointY + increment * 2 );
-    segment2.stroke = "black";
-    segment2.linewidth = 2;
+    for (let i in points) {
+        currentPoint = points[Number(i)];
+        nextPoint = points[Number(i) + 1];
+        if (nextPoint) {
+            const segment = two.makeLine(currentPoint.x, currentPoint.y, nextPoint.x, nextPoint.y);
+            segment.stroke = "black";
+            segment.linewidth = 2;
+        }
+    }
 
-    const dotMarker = two.makeCircle(centerpointX, centerpointY + increment * 2, 12);
+    return currentPoint;
+
+}
+
+function drawMoveableSpots(fromCoord, increment) {
+    const points = [];
+    const MOVEABLE_CIRCLE_RADIUS = 8;
+
+    points.push(
+        two.makeCircle(fromCoord.x - increment, fromCoord.y, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x + increment, fromCoord.y, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x, fromCoord.y - increment, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x, fromCoord.y + increment, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x - increment, fromCoord.y - increment, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x - increment, fromCoord.y + increment, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x + increment, fromCoord.y + increment, MOVEABLE_CIRCLE_RADIUS),
+        two.makeCircle(fromCoord.x + increment, fromCoord.y - increment, MOVEABLE_CIRCLE_RADIUS),
+    );
+
+    two.update();
+
+    points.forEach(p => {
+        p._renderer.elem.addEventListener('mouseover', function () {
+            p.fill = "lightblue";
+            p.opacity = 0.6;
+            two.update();
+        });
+        p._renderer.elem.addEventListener('mouseout', function () {
+            p.fill = "white";
+            p.opacity = 1;
+            two.update();
+        });
+        p._renderer.elem.addEventListener('click', function () {
+            // alert(p._translation.x + " " + p._translation.y);
+            pathPoints.push({x: p._translation.x, y: p._translation.y});
+            renderPath();
+        });
+    });
+    moveablePoints = [...points];
+}
+
+function renderPath(){
+    const currentDot = drawLinePath(pathPoints, 50);
+    two.remove(moveablePoints);
+    two.remove(dotMarker);
+    moveablePoints = [];
+    drawMoveableSpots(currentDot, 50);
+    dotMarker = two.makeCircle(currentDot.x, currentDot.y, 12);
     dotMarker.fill = "yellow";
-
+    two.update();
 }
+
+const centerpoint = getCenterPoint(10, 8, 50);
+const pathPoints = [centerpoint];
+
+let moveablePoints = [];
+let dotMarker = null;
+drawMoveableSpots(centerpoint, 50);
+
+const pointMap = {};
 
 drawGraphPaper(10, 8, 50);
-drawStartDot(10, 8, 50);
 drawPitch(10, 8, 50);
-drawLineSegment(10, 8, 50);
-two.update();
+drawStartDot(centerpoint);
+
+renderPath();

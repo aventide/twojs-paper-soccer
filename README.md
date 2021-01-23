743e54673d4684479b29fdfcbac57127a9bb7614
Alex Staples <ajstaples@gmail.com>
2021-01-23 16:15:28 -0500

    probably the final cleanup

--- README.md
+++ README.md
@@ -12,6 +12,34 @@ yarn install
 yarn start
 
 See 'Possible Issues' below if running on Apple Silicon machines causes issues
+
+## Code elements
+
+### Rules
+
+Functions that return game state based on conditions present on the game object, or conditions present in a temporary object
+these rule functions will come in handy when it's time to do server-side validation of game moves
+
+### Renderer - a function that draws graphics to the two.js canvas based on existing game state.
+
+Routines, meaning that they function by having side effects. They could have a return value that is a status, if need be.
+
+renderers need to know the following values to work:
+
+1. anchorpoint, to know where to draw
+2. edgeLength, to handle responsiveness
+3. access to game state, to know what objects to draw and where
+
+(Currently, renderers all have access to the gloabl game object)
+
+So renderer functions in this game can assume they recieve these params:
+```
+anchor: {x, y}
+edgeLength: x
+state: {
+    ...state generating functions, aka rules.
+}
+```
 ## Game Lifecycle
 
 1. Initial config
--- renderers.js
+++ renderers.js
@@ -1,26 +1,3 @@
-/* renderer - a function that draws graphics to the two.js canvas based on existing game state.
-
-renderers are routines, meaning that they function by having side effects. They could have a return value that is a status, if need be.
-
-renderers need to know the following values to work:
-
-1. anchorpoint, to know where to draw
-2. edgeLength, to handle responsiveness
-3. access to game state, to know what objects to draw and where
-
-So renderer functions in this game can assume they recieve these params:
-
-anchor: {x, y}
-edgeLength: x
-state: {
-    ...state generating functions, aka rules.
-}
-
-rules are functions that return game state based on conditions present on the game object, or conditions present in a temporary object
-these rule functions will come in handy when it's time to do server-side validation of game moves
-
-*/
-
 import {
   NUMBER_ROWS,
   NUMBER_COLS,
@@ -305,10 +282,6 @@ export function renderMoveableSpots(game) {
 }
 
 export function renderPitch(game) {
-  // @todo is this necessary?
-  game.two.clear();
-
-  // do we really need both of these args though? They seem really global...refactor this later, you asshole.
   game.handles.graphPaper = renderGraphPaper(game);
   game.handles.pitchBorders = renderPitchBorders(game);
   game.handles.startPositionDot = renderStartDot(game);
